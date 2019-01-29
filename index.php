5574206c3d1e72a54c8dbc7734e157be54c21642
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-29 12:58:05 -0500

    added styles to most pages and updated views to templates

--- index.php
+++ index.php
@@ -16,35 +16,36 @@ $f3 = Base::instance();
 //turn on fat-free error reporting
 $f3->set('DEBUG', 3);
 
+//arrays for interests
+$f3->set('indoor', array('tv', 'movies', 'cooking', 'board games',
+            'puzzles', 'reading', 'playing cards', 'video games'));
+$f3->set('outdoor', array('hiking', 'biking', 'swimming', 'collecting',
+                        'walking'), 'climbing');
+
+
+
 //define a default route
-$f3->route('GET /', function() {
-    //echo "<h1>My Dating Website</h1>";
-    $view = new View;
-    echo $view->render('views/home.html');
+$f3->route('GET|POST /', function() {
+    $template = new Template;
+    echo $template->render('views/home.html');
 });
-
-
-
-
-
-
 
 //define a personal info view
 $f3->route('GET|POST /personalInfo', function() {
-    $view = new View();
-    echo $view->render('views/personalInfo.html');
+    $template = new Template();
+    echo $template->render('views/personalInfo.html');
 });
 
 //define a profile view
 $f3->route('GET|POST /profile', function() {
-    $view = new View();
-    echo $view->render('views/profile.html');
+    $template = new Template();
+    echo $template->render('views/profile.html');
 });
 
 //define an interests view
 $f3->route('GET|POST /interests', function() {
-    $view = new View();
-    echo $view->render('views/interests.html');
+    $template = new Template();
+    echo $template->render('views/interests.html');
 });
 
 
